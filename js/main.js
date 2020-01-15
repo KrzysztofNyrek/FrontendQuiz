a2b48eeab37c71bdc91ce833df331ebe6aec7a10
Krzysztof Nyrek <nitrobenzen@gmail.com>
2020-01-15 17:55:43 +0100

    Set session storage mehanizm

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 "use strict"
 
+//Used sesion storage to keep Total Score value
+let totalScore = JSON.parse(sessionStorage.getItem("SesionTotalScore"));
+
+if(totalScore === null){
+  sessionStorage.setItem("SesionTotalScore", 0);
+}
+
+
 //Question storage section
 function Question(questionValue, answer1, answer2, answer3, correctAnswer, correctAnswerMessage, wrongAnswerMessage){
   this.questionValue = questionValue;
@@ -21,6 +29,7 @@ let question = document.querySelector(".question--js");
 let ans1 = document.querySelector(".answer__choice1--js");
 let ans2 = document.querySelector(".answer__choice2--js");
 let ans3 = document.querySelector(".answer__choice3--js");
+let correctAnswerNumber;
 
 //Question select
 switch (randomNumber){
@@ -29,12 +38,14 @@ switch (randomNumber){
     ans1.textContent = question1.answer1;
     ans2.textContent = question1.answer2;
     ans3.textContent = question1.answer3;
+    correctAnswerNumber = question1.correctAnswer;
     break;
   case 2:
     question.textContent = question2.questionValue;
     ans1.textContent = question2.answer1;
     ans2.textContent = question2.answer2;
     ans3.textContent = question2.answer3;
+    correctAnswerNumber = question2.correctAnswer;
   break;
 };
 //Answer service
@@ -43,5 +54,9 @@ let answerButton = document.querySelector(".button--js");
 answerButton.addEventListener('click', (e) =>{
 
   let answerValue = document.querySelector('input[name="answerX"]:checked').value;
-
+  if (answerValue == correctAnswerNumber){
+    totalScore += 1;
+    sessionStorage.setItem('SesionTotalScore', totalScore);
+    alert(totalScore);
+  }
 });
