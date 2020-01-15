32dd96ad2812f9db7e44318c4846aeb76038d519
Krzysztof Nyrek <nitrobenzen@gmail.com>
2020-01-15 18:21:45 +0100

    changed math.floor to math.round

--- js/main.js
+++ js/main.js
@@ -24,7 +24,7 @@ let question1 = new Question('Czy JS jest super?', 'Tak', 'Nie', 'Nie wiem', 1, 
 let question2 = new Question('Jak wyśrodkować element na stronie?', 'margin: 0 auto', 'text-adjusting: center', 'div: center', 2, 'Masz rację, otrzymujesz 1 punkt.', 'Niestety nie. Prawidłowa odpowiedź brzmi: margin: 0 auto');
 
 //Build question section
-let randomNumber = Math.floor(Math.random() * 1 + 1);
+let randomNumber = Math.round(Math.random() * 1);
 let question = document.querySelector(".question--js");
 let ans1 = document.querySelector(".answer__choice1--js");
 let ans2 = document.querySelector(".answer__choice2--js");
@@ -33,14 +33,14 @@ let correctAnswerNumber;
 
 //Question select
 switch (randomNumber){
-  case 1:
+  case 0:
     question.textContent = question1.questionValue;
     ans1.textContent = question1.answer1;
     ans2.textContent = question1.answer2;
     ans3.textContent = question1.answer3;
     correctAnswerNumber = question1.correctAnswer;
     break;
-  case 2:
+  case 1:
     question.textContent = question2.questionValue;
     ans1.textContent = question2.answer1;
     ans2.textContent = question2.answer2;
