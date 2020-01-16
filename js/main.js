7d405a55d000e97b912a1dcc6f359b4c6e05306e
Krzysztof Nyrek <nitrobenzen@gmail.com>
2020-01-16 13:43:16 +0100

    Set behavior on good and wrong answer

--- js/main.js
+++ js/main.js
@@ -4,12 +4,12 @@
 //Variables definitions
 let totalScore = JSON.parse(sessionStorage.getItem("SesionTotalScore"));
 let answerButton = document.querySelector(".button--js");
-let correctAnswerBox = document.querySelector(".correct-answer--js");
-let wrongAnswerBox = document.querySelector(".wrong-answer--js");
+let AnswerBox = document.querySelector(".answer-box--js");
+let answerBoxText = document.querySelector(".answer-box__text--js");
 let finalResultBox = document.querySelector(".final-result--js");
-let partScoreDisplay = document.querySelector(".box-score--js");
-let moveOnButton = document.querySelector(".box-button--js");
-let temporaryAnswer = JSON.parse(sessionStorage.getItem("TemporaryAnswer")); //keep value true/false to load correctAnswerBox or wrongAnswerBox
+let partScoreDisplay = document.querySelector(".answer-box__score--js");
+let moveOnButton = document.querySelector(".answer-box__button--js");
+let temporaryAnswer = JSON.parse(sessionStorage.getItem("TemporaryAnswer")); //keep value true/false to load AnswerBox or wrongAnswerBox
 
 if(totalScore === null){
   sessionStorage.setItem("SesionTotalScore", 0);
@@ -20,11 +20,17 @@ let totalQuestionNumber = JSON.parse(sessionStorage.getItem("SesionTotalQuestion
 if(totalQuestionNumber === null){
   sessionStorage.setItem("SesionTotalQuestion", 0);
 
-}else if(totalQuestionNumber >0 && totalQuestionNumber <10 && temporaryAnswer == 1){
-  correctAnswerBox.classList.remove('correct-answer--visible');
+}else if(totalQuestionNumber >0 && totalQuestionNumber <10){
+  AnswerBox.classList.remove('answer-box--visible');
+  
+    if(temporaryAnswer == 1){
+      answerBoxText.textContent = "Śweitna odpowiedź";
+    }else{
+      answerBoxText.textContent = "Tym razem pójdzie lepiej ;)"
+    }
   partScoreDisplay.textContent = totalScore;
   moveOnButton.addEventListener('click', (e) =>{
-    correctAnswerBox.classList.add('correct-answer--visible');
+    AnswerBox.classList.add('answer-box--visible');
   });
 
 }else if(totalQuestionNumber == 10){
@@ -40,13 +46,10 @@ function Question(questionValue, answer1, answer2, answer3, correctAnswer, corre
   this.answer2 = answer2;
   this.answer3 = answer3;
   this.correctAnswer = correctAnswer;
-  this.correctAnswerMessage = correctAnswerMessage;
-  this.wrongAnswerMessage = wrongAnswerMessage;
 }
 
-let question1 = new Question('Czy JS jest super?', 'Tak', 'Nie', 'Nie wiem', 1, 'Masz rację, otrzymujesz 1 punkt.', 'Niestety nie. Prawidłowa odpowiedź brzmi: JS jest super');
-
-let question2 = new Question('Jak wyśrodkować element na stronie?', 'margin: 0 auto', 'text-adjusting: center', 'div: center', 2, 'Masz rację, otrzymujesz 1 punkt.', 'Niestety nie. Prawidłowa odpowiedź brzmi: margin: 0 auto');
+let question1 = new Question('Czy JS jest super?', 'Tak', 'Nie', 'Nie wiem', 1);
+let question2 = new Question('Jak wyśrodkować element na stronie?', 'margin: 0 auto', 'text-adjusting: center', 'div: center', 2);
 
 //Build question section
 let randomNumber = Math.round(Math.random() * 1);
@@ -87,5 +90,7 @@ answerButton.addEventListener('click', (e) =>{
     totalScore += 1;
     sessionStorage.setItem('SesionTotalScore', totalScore);
     sessionStorage.setItem('TemporaryAnswer', 1);
+  }else{
+    sessionStorage.setItem('TemporaryAnswer', 0);
   }
 });
