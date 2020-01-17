c38c1377f692c42a3555d537cba774ef5ee55d51
Krzysztof Nyrek <nitrobenzen@gmail.com>
2020-01-17 08:20:19 +0100

    Refactored code and add comments

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
 "use strict"
 
-
 //Variables definitions
-let totalScore = JSON.parse(sessionStorage.getItem("SesionTotalScore"));
 let answerButton = document.querySelector(".button--js");
 let AnswerBox = document.querySelector(".answer-box--js");
 let answerBoxText = document.querySelector(".answer-box__text--js");
@@ -11,18 +9,21 @@ let partScoreDisplay = document.querySelector(".answer-box__score--js");
 let finalScoreDisplay = document.querySelector(".final__score--js");
 let moveOnButton = document.querySelector(".answer-box__button--js");
 let finalButton = document.querySelector(".final-result__button--js");
-let temporaryAnswer = JSON.parse(sessionStorage.getItem("TemporaryAnswer")); //keep value true/false to load AnswerBox or wrongAnswerBox
+
 let finalyComment = document.querySelector(".final-result__summary--js");
 
+//Used session storage to avoid losing data when browser refreshing page
+let totalScore = JSON.parse(sessionStorage.getItem("SessionTotalScore"));
+let temporaryAnswer = JSON.parse(sessionStorage.getItem("TemporaryAnswer")); //keep value true/false to load AnswerBox or wrongAnswerBox
+let totalQuestionNumber = JSON.parse(sessionStorage.getItem("SessionTotalQuestion"));
+
+// Set Session Total Score variable in session storage
 if(totalScore === null){
-  sessionStorage.setItem("SesionTotalScore", 0);
+  sessionStorage.setItem("SessionTotalScore", 0);
 }
-
-//Used sesion storage to keep Total number of question
-let totalQuestionNumber = JSON.parse(sessionStorage.getItem("SesionTotalQuestion"));
-
+//Check if it's first round or another round and display popup box 
 if(totalQuestionNumber === null){
-  sessionStorage.setItem("SesionTotalQuestion", 0);
+  sessionStorage.setItem("SessionTotalQuestion", 0);
 
 }else if(totalQuestionNumber >0 && totalQuestionNumber <10){
   AnswerBox.classList.remove('answer-box--visible');
@@ -30,28 +31,29 @@ if(totalQuestionNumber === null){
     if(temporaryAnswer === 1){
       answerBoxText.textContent = "Świetna odpowiedź";
     }else{
-      answerBoxText.textContent = "Tym razem pójdzie lepiej ;)"
+      answerBoxText.textContent = "Następnym razem pójdzie Ci lepiej ;)"
     }
   partScoreDisplay.textContent = totalScore;
   moveOnButton.addEventListener('click', (e) =>{
     AnswerBox.classList.add('answer-box--visible');
   });
-
+//Display final result box
 }else if(totalQuestionNumber === 10){
   finalResultBox.classList.remove('final-result--visible');
 
   if(totalScore < 5){
-    finalyComment.textContent ="Jeśli poćwiczysz jeszcze trochę, na pewno będziesz znal odpowiedź na większość pytań";
+    finalyComment.textContent ="Jeśli poćwiczysz jeszcze trochę, na pewno będziesz znał odpowiedź na większość pytań";
   }else if (totalScore >= 5 && totalScore < 8){
     finalyComment.textContent = "Już prawie wiesz wszystko :)";
   }else{
     finalyComment.textContent = "Jesteś super FrontEnd HERO";
   }
   finalScoreDisplay.textContent = totalScore;
+  //Refresh page to delete all session storage
   finalButton.addEventListener('click', (e) =>{
     finalResultBox.classList.add('final-result--visible');
-    sessionStorage.setItem("SesionTotalQuestion", 0);
-    sessionStorage.setItem("SesionTotalScore", 0);
+    sessionStorage.setItem("SessionTotalQuestion", 0);
+    sessionStorage.setItem("SessionTotalScore", 0);
     window.location.reload(false);
   });
 }
@@ -64,12 +66,13 @@ function Question(questionValue, answer1, answer2, answer3, correctAnswer, corre
   this.answer3 = answer3;
   this.correctAnswer = correctAnswer;
 }
-
+//Question value
 let question1 = new Question('Czy JS jest super?', 'Tak', 'Nie', 'Nie wiem', 1);
 let question2 = new Question('Jak wyśrodkować div na stronie?', 'margin: 0 auto', 'text-adjusting: center', 'div: center', 1);
 let question3 = new Question('Jaką wartość box-sizing trzeba ustawić w CSS, żeby padding nie powiększył wymiarów box\'a', 'content-box', 'border-box', 'inherit', 2);
 let question4 = new Question('Jaki znacznik HTML pozwala bez użycia CSS dopasować rozmiar obrazu do wymiaru ekranu?', 'img', 'iframe', 'picture', 3);
-//Build question section
+
+//Variables definitions
 let question = document.querySelector(".question--js");
 let ans1 = document.querySelector(".answer__choice1--js");
 let ans2 = document.querySelector(".answer__choice2--js");
@@ -82,8 +85,9 @@ let questionStorageNbr = [];
 let testValue;
 let questionStr;
 let questionNumber;
-
 let questionHistory = sessionStorage.getItem("QuestionHistory");
+
+//Question pick mechanism
 if(questionHistory === null){
   randomNumber = Math.round(Math.random() * 3);
   sessionStorage.setItem("QuestionHistory", randomNumber);
@@ -98,7 +102,7 @@ if(questionHistory === null){
       questionNumber = Number(questionStr);
       questionStorageNbr.push(questionNumber);
     }
-
+  //Check if question appeared in current quiz earlier and if so change the question number
   do{
     randomNumber = Math.round(Math.random() * 3);
     testValue = 0;
@@ -112,7 +116,7 @@ if(questionHistory === null){
   questionStorageNbr.push(randomNumber);
   sessionStorage.setItem("QuestionHistory", questionStorageNbr);
 }
-//Question select
+//Push question value to the browser
 switch (randomNumber){
   case 0:
     question.textContent = question1.questionValue;
@@ -143,19 +147,19 @@ switch (randomNumber){
     correctAnswerNumber = question4.correctAnswer;
   break;
 };
-//Answer service
 
+//Send answer and count points
 answerButton.addEventListener('click', (e) =>{
   
-  //Increas number of question
+  //Increas and storage number of question
   totalQuestionNumber += 1;
-  sessionStorage.setItem('SesionTotalQuestion', totalQuestionNumber);
+  sessionStorage.setItem('SessionTotalQuestion', totalQuestionNumber);
 
   //Checked if answer is good and added point if so
   let answerValue = document.querySelector('input[name="answerX"]:checked').value;
   if (answerValue == correctAnswerNumber){
     totalScore += 1;
-    sessionStorage.setItem('SesionTotalScore', totalScore);
+    sessionStorage.setItem('SessionTotalScore', totalScore);
     sessionStorage.setItem('TemporaryAnswer', 1);
   }else{
     sessionStorage.setItem('TemporaryAnswer', 0);
